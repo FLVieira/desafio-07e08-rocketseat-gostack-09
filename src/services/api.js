5d1c271cc8a844f060f8187684a4a07f6b19264b
Fábio Vieira <fabioluizvieiraf@gmail.com>
2020-06-10 00:59:31 -0300

    Enviando para o Github

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   headers: { 'Content-Type': 'application/json' },
-  baseURL: 'http://192.168.1.15:3001/',
+  baseURL: 'http://192.168.1.8:3001/',
 });
 
 export default api;
